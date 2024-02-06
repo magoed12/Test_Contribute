98035642f780e011baa2c1cdaffa984afbc2391a
magoed12 <107357062+magoed12@users.noreply.github.com>
2024-02-06 11:57:00 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace test
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            TempConvert T = new TempConvert();
+            Console.WriteLine($"Celcius = {T.C} Farenheit = {T.F}");
+            double answer = (32 - T.F) * (5.0 / 9.0);
+            Console.WriteLine($"F = {T.F}, C = {answer}");
+        }
+    }
+    class TempConvert
+    {
+        public double C = 0.0;
+        public double F = 32;
+    }
+}
